c1cc0ce5cdf3dc8eceb8774282612307678f261f
MarcosFelipeDSilva <marcos.felipe01@hotmail.com>
2021-08-02 19:25:56 -0300

    first commit

---
+++ src/main/java/com/crud/pessoa/crud_test_pessoa/CrudTestPessoaApplication.java
@@ -0,0 +1,13 @@
+package com.crud.pessoa.crud_test_pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTestPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTestPessoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/pessoa/crud_test_pessoa/CrudTestPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.pessoa.crud_test_pessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTestPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
